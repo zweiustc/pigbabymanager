8ce5e999fed11878166987a5324e2cb0ce196d46
zhangwei18 <zhangwei18@kingsoft.com>
2017-01-09 21:57:39 +0800

    add create mysql db table case
    We use 'python manage.py makemigrations' and 'python manage.py migrate'
    to sync database.

--- pigbabymanager/pigbabymanager/settings.py
+++ pigbabymanager/pigbabymanager/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pigs',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,14 @@ WSGI_APPLICATION = 'pigbabymanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pigmanager',
+        'USER': 'root',
+        'PASSWORD': 'zhangwei',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- pigbabymanager/pigs/models.py
+++ pigbabymanager/pigs/models.py
@@ -3,3 +3,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Pigs(models.Model):
+    uuid = models.CharField(max_length=36)
