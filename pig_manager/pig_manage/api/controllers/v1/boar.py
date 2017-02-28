dd16b647a49f94c0929eb0fa9cb6f08a056d0a1e
ZhangWei <zweiustc@gmail.com>
2017-02-28 23:27:02 +0800

    fix a spelling error

--- pig_manager/pig_manage/api/controllers/v1/boar.py
+++ pig_manager/pig_manage/api/controllers/v1/boar.py
@@ -11,47 +11,47 @@ from pig_manage.common import exception
 from pig_manage import objects
 
 
-class Boar(base.APIBase):
-    id = wtypes.text
-    ear_tag = wtypes.text
-    ear_lack = wtypes.text
-    #birthday = wtypes.datetime
-    #entryday = wtypes.datetime
-    birthday = wtypes.text
-    entryday = wtypes.text
-
-    dormitory_id = wtypes.text
-    category_id = wtypes.text
-    breed_num = wtypes.IntegerType
-    breed_acceptability = wtypes.text
-    source_id = wtypes.text
-
-    def __init__(self, **kwargs):
-        self.fields = []
-        for field in objects.Boar.fields:
-            if not hasattr(self, field):
-                continue
-            self.fields.append(field)
-            setattr(self, field, kwargs.get(field, wtypes.Unset))
-
-    @classmethod
-    def convert(self, boar):
-        return Boar(**boar.as_dict())
-
-
-class BoarCollection(collection.Collection):
-
-    boars = [Boar]
-
-    def __init__(self, **kwargs):
-        self._type = 'boars'
-
-    @staticmethod
-    def convert(boars):
-        collection = BoarCollection()
-        collection.boars = [Boar.convert(p)
-                for p in boars]
-        return collection
+#class Boar(base.APIBase):
+#    id = wtypes.text
+#    ear_tag = wtypes.text
+#    ear_lack = wtypes.text
+#    #birthday = wtypes.datetime
+#    #entryday = wtypes.datetime
+#    birthday = wtypes.text
+#    entryday = wtypes.text
+#
+#    dormitory_id = wtypes.text
+#    category_id = wtypes.text
+#    breed_num = wtypes.IntegerType
+#    breed_acceptability = wtypes.text
+#    source_id = wtypes.text
+#
+#    def __init__(self, **kwargs):
+#        self.fields = []
+#        for field in objects.Boar.fields:
+#            if not hasattr(self, field):
+#                continue
+#            self.fields.append(field)
+#            setattr(self, field, kwargs.get(field, wtypes.Unset))
+#
+#    @classmethod
+#    def convert(self, boar):
+#        return Boar(**boar.as_dict())
+#
+#
+#class BoarCollection(collection.Collection):
+#
+#    boars = [Boar]
+#
+#    def __init__(self, **kwargs):
+#        self._type = 'boars'
+#
+#    @staticmethod
+#    def convert(boars):
+#        collection = BoarCollection()
+#        collection.boars = [Boar.convert(p)
+#                for p in boars]
+#        return collection
 
 
 class BoarsController(rest.RestController):
@@ -88,7 +88,7 @@ class BoarsController(rest.RestController):
                 pecan.request.context)
         import pdb; pdb.set_trace()
         result = [self._format_boar(boar) for boar in boars]
-        return {'boar': result}
+        return {'boars': result}
         #return BoarCollection.convert(boars)
 
     def post(self, network):
